10c1b99e69fb4e76f15b18e8dd909b27cb330154
AsheKR <tech@ashe.kr>
2021-01-15 01:09:11 +0900

    Add AWS Route53 Setting

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 provider "heroku" {}
+provider "aws" {
+	region = var.aws_region
+	access_key = var.aws_access_key
+	secret_key = var.aws_secret_key
+}
 
 terraform {
 	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+		}
 		heroku = {
 			source = "heroku/heroku"
 		}
@@ -23,4 +31,12 @@ resource "heroku_app" "heroku_instance" {
 resource "heroku_addon" "database" {
 	app = heroku_app.heroku_instance.name
 	plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "aws_route53_record" "route53" {
+	zone_id = var.route53_zone_id
+	name = var.subdomain_name
+	type = "CNAME"
+	ttl = "300"
+	records = ["${heroku_app.heroku_instance.heroku_hostname}"]
 }
--- variables.tf
+++ variables.tf
@@ -14,3 +14,29 @@ variable "heroku_email" {
 	type = string
 	default = ""
 }
+
+variable "aws_region" {
+        type = string
+	description = "AWS region to create the VPS and services"
+        default = "ap-northeast-2"
+}
+
+variable "aws_access_key" {
+        type = string
+	description = "AWS access key associated with an IAM user or role"
+}
+
+variable "aws_secret_key" {
+        type = string
+	description = "The secret key associated with the access key. This is essentially the `password` for the access key"
+}
+
+variable "subdomain_name" {
+        type = string
+        description = "The DNS subdomain name where the server will be available"
+}
+
+variable "route53_zone_id" {
+        type = string
+        description = "Route 53 zone ID to create new DNS records within"
+}
